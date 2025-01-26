a973347a70591274063e4787b86f3202fc1bb5e5
adina778899 <adinamemetea@yahoo.com>
2025-01-26 16:27:17 -0600

    Initial Commit

---
+++ Adding image lesson7/Adding_image_lesson7App.swift
@@ -0,0 +1,17 @@
+//
+//  Adding_image_lesson7App.swift
+//  Adding image lesson7
+//
+//  Created by natural health on 1/26/25.
+//
+
+import SwiftUI
+
+@main
+struct Adding_image_lesson7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
